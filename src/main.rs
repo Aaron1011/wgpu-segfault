2386a44cffb8c7b7e9f150db37d4eb23a99a9316
Aaron Hill <aa1ronham@gmail.com>
2021-02-20 13:52:38 -0500

    Narrow things down

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
 fn run() {
     wgpu_core::hub::Global::new(
             "wgpu",
             wgpu_core::hub::IdentityManagerFactory,
             wgpu_types::BackendBit::all()
     );
+    println!("Thread exiting!");
 }
 
 fn main() {
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::TRACE)
+        // builds the subscriber.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
+
     std::thread::spawn(|| {
         run();
     }).join().unwrap();
+    println!("Sleeping!");
+    std::thread::sleep(std::time::Duration::new(2, 0));
 }
