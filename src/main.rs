44c99e7e1fb20d069205c821d801022efa8facdd
Aaron Hill <aa1ronham@gmail.com>
2021-02-20 14:31:03 -0500

    Minimize things

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,11 @@
 use gfx_hal::Instance;
-use env_logger::Builder;
-use log::LevelFilter;
 
 fn run() {
-    /*wgpu_core::hub::Global::new(
-            "wgpu",
-            wgpu_core::hub::IdentityManagerFactory,
-            wgpu_types::BackendBit::all()
-    );*/
-    gfx_backend_gl::Instance::create("wgpu", 1);
+    gfx_backend_gl::Instance::create("wgpu", 1).unwrap();
     println!("Thread exiting!");
 }
 
 fn main() {
-
-    let mut builder = Builder::new();
-    builder.filter_level(LevelFilter::Trace);
-    builder.init();
-
-
     std::thread::spawn(|| {
         run();
     }).join().unwrap();
